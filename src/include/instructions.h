3fb3de80879f3cc3398ad66db775e80903c3983a
adwashington <adwashington123@gmail.com>
2024-02-15 11:51:59 -0600

    Added instruction formats.

--- src/include/instructions.h
+++ src/include/instructions.h
@@ -37,6 +37,64 @@ enum addressing_modes {
     None
 };
 
+enum instruction_flags {
+    IF_High_Byte       = 3,
+    IF_Sign            = 2,
+    IF_Direction       = 1,
+    IF_Size            = 6,
+    IF_High_Byte_Dst   = 4,
+    IF_High_Byte_Base  = 3,
+    IF_High_Byte_Index = 0,
+    IF_High_Byte_Src   = 7,
+    IF_Extension       = 5,
+};
+
+struct instruction_format_immd8 {
+    uint8_t dest;
+    uint8_t immd8;
+    uint8_t flags;
+};
+
+struct instruction_format_immd16 {
+    uint8_t dest;
+    uint16_t immd16;
+    uint8_t flags;
+};
+
+struct instruction_format_pc_rel {
+    uint8_t dest;
+    int16_t sImmd8;
+    uint8_t flags;
+};
+
+struct instruction_format_abs {
+    uint8_t dest;
+    uint16_t immd16;
+};
+
+struct instruction_format_ind {
+    uint8_t dest;
+    uint8_t base;
+    uint8_t index;
+    int16_t sImmd16;
+    uint8_t flags;
+};
+
+struct instruction_format_reg {
+    uint8_t dest;
+    uint8_t src;
+    uint8_t flags;
+};
+
+union instruction_format {
+  struct instruction_format_immd8  immd8;
+  struct instruction_format_immd16 imm16;
+  struct instruction_format_pc_rel pc_rel;
+  struct instruction_format_abs    abs;
+  struct instruction_format_ind    ind;
+  struct instruction_format_reg    reg;
+};
+
 struct instruction {
     enum opcode opcode;
     char *mnemonic;
